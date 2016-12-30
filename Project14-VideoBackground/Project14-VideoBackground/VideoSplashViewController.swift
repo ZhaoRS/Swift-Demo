7356778096e5590c4d87f3f3a5f588c1b3b85fe0
_涤_ <zhao_ruisheng@126.com>
2016-12-30 16:04:47 +0800

    Project14-VideoBackground

---
+++ Project14-VideoBackground/Project14-VideoBackground/VideoSplashViewController.swift
@@ -0,0 +1,9 @@
+//
+//  VideoSplashViewController.swift
+//  Project14-VideoBackground
+//
+//  Created by 赵瑞生 on 2016/12/29.
+//  Copyright © 2016年 赵瑞生. All rights reserved.
+//
+
+import Foundation
---
+++ Project14-VideoBackground/Project14-VideoBackground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project14-VideoBackground
+//
+//  Created by 赵瑞生 on 2016/12/29.
+//  Copyright © 2016年 赵瑞生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
